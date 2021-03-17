b9748149a67bcd227c22d5bd5ec96ef91ced7ba2
Garrett Mulroney <gmulroney@gmail.com>
2021-03-17 13:43:18 -0400

    added initial view structure

--- AssessmentProject/AccountInfoViewController.swift
+++ AssessmentProject/AccountInfoViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AccountInfoViewController.swift
 //  AssessmentProject
 //
 //  Created by Garrett Mulroney on 3/17/21.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class AccountInfoViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+    }
 }
-
---
+++ AssessmentProject/TransactionListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionListViewController.swift
+//  AssessmentProject
+//
+//  Created by Garrett Mulroney on 3/17/21.
+//
+
+import UIKit
+
+class TransactionListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+    }
+}
